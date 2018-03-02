1b12779b5b43db62bd60bcf2043f627af3852680
brandonstranzl <brandonstranzl@gmail.com>
2018-03-02 20:56:37 +0000

    added user signup and login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :users
+  resources :sessions
+  get '/logout' => 'sessions#destroy'
+  post '/login' => 'sessions#create'
+  get '/login' => 'sessions#new'
 
   resource :cart, only: [:show] do
     put    :add_item
@@ -15,8 +20,11 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show, :destroy]
+    resources :categories, except: [:edit, :update, :show]
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
