7aea844c7542402bbae0da8db43ba71e0d5fac6a
brandonstranzl <brandonstranzl@gmail.com>
2018-03-04 21:19:34 +0000

    now added second way to acheive if cart empty, show message that it is empty, plus link to homepage. first time, used server to simply redirect home, and add message to home page. this time, used html with erb to either insert the cart or insert a message plus a link.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,9 @@
 class CartsController < ApplicationController
 
   def show
-    
-    if cookies[:cart] == "{}"
-    redirect_to '/', notice: "Your cart is empty! Please add to it by shopping. Click below:"
-    end
+    # if cookies[:cart] == "{}"
+    # redirect_to '/', notice: "Your cart is empty! Please add to it by shopping. Click below:"
+    # end
   end
 
   def add_item
